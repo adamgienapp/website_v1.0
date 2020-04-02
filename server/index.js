e13cb24c1271d813e8e086f5a2c5b8c1ea69ef10
Adam <gienappa@gmail.com>
2020-04-02 11:35:16 -0700

    prelim server complete

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const port = 3000;
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+app.use(cors());
+
+app.use(express.static(__dirname + '/../client/dist'));
+app.listen(port, () => `Server listening on port ${port}`);
