874e39900658e73bf1eddaa3b2a872c1d0eb0250
Adam <gienappa@gmail.com>
2020-04-12 20:56:37 -0700

    revise pdf embed to object tag
    styling added to improve mobile response

--- client/src/components/Resume.jsx
+++ client/src/components/Resume.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Resume = (props) => {
   return (
     <div className="resume-page">
-      <object data="./adam-gienapp-resume.pdf#view=FitH" type="application/pdf" width="100%" height="1200px" style={{display: 'block'}}/>
+      <object data="./adam-gienapp-resume.pdf#view=FitH" type="application/pdf" className="resume-obj"/>
     </div>
   );
 }
