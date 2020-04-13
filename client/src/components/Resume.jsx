4fcb3a1a7f41d0a9c4485fd10786cc776658d680
Adam <gienappa@gmail.com>
2020-04-12 20:42:55 -0700

    meta viewport set to enhance mobile support

--- client/src/components/Resume.jsx
+++ client/src/components/Resume.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Resume = (props) => {
   return (
     <div className="resume-page">
-      <embed src="./adam-gienapp-resume.pdf#view=FitH" height="800px" width="100%" style={{display: 'block'}}/>
+      <object data="./adam-gienapp-resume.pdf#view=FitH" type="application/pdf" width="100%" height="1200px" style={{display: 'block'}}/>
     </div>
   );
 }
