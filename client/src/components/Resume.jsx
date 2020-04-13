3edb6d34149f3ea8acf06612f0aae45a882cdd78
Adam <gienappa@gmail.com>
2020-04-12 21:12:18 -0700

    pdf sizing revised
    mobile issue - width appears "scrunched"

--- client/src/components/Resume.jsx
+++ client/src/components/Resume.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const Resume = (props) => {
   return (
     <div className="resume-page">
-      <object data="./adam-gienapp-resume.pdf#view=FitH" type="application/pdf" className="resume-obj"/>
+      <object data="./adam-gienapp-resume.pdf#view=FitH" type="application/pdf" className="resume-obj">
+        <p>Oops! Your browser doesn't support PDFs!</p>
+        <p><a href="/adam-gienapp-resume.pdf">Download Instead</a></p>
+      </object>
     </div>
   );
 }
