77e2cea48ca8f0b672e5574c3fa11a1e087a2a03
Adam <gienappa@gmail.com>
2020-06-20 12:32:34 -0700

    style.css: - changed breakpoint of new-line appearance in project anchors
    Projects:
    - added anchor for ScratchTrack
    - changed order of projects from oldest-first to newest-first

--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -4,13 +4,15 @@ import React from 'react';
 const Projects = (props) => {
   return (
     <div className="projects-page container">
+
       <div id="top"/>
+
       <div className="projects-anchors">
         <div>
-          <a href="#FEC">
-            <span className="project-anchor-text">All Day I Dream</span>
+          <a href="#scratchTrack">
+            <span className="project-anchor-text">ScratchTrack</span>
             <span className="project-anchor-space">&nbsp;</span>
-            <span className="project-anchor-text">About Servers</span>
+            <span className="project-anchor-text">Web App</span>
           </a>
         </div>
         <div>
@@ -20,37 +22,21 @@ const Projects = (props) => {
             <span className="project-anchor-text">System Design</span>
           </a>
         </div>
+        <div>
+          <a href="#FEC">
+            <span className="project-anchor-text">All Day I Dream</span>
+            <span className="project-anchor-space">&nbsp;</span>
+            <span className="project-anchor-text">About Servers</span>
+          </a>
+        </div>
         {/* <div>
           <a href="#encode32">Encode32</a>
         </div> */}
       </div>
+
       <hr></hr>
+
       <div className="projects-list">
-        <div id="FEC"/>
-        <div className="project-item">
-          <div className="project-title bg-dark">
-            <h4>All Day I Dream About Servers</h4>
-            <h6 className="project-byline">An adidas product detail page mock-up</h6>
-          </div>
-          <div className="project-image">
-            <img className="project-image" src="https://i.imgur.com/Sc7qASD.gif" alt="adidas product detail page demo" text="adidas product detail page demo" width="100%"></img>
-          </div>
-          <div className="project-description">
-            <div className="project-tech">
-              <span className="tech-title">Tech stack</span> | React, Node + Express, MySQL + Sequelize, Jest + Enzyme, CircleCI
-            </div>
-            <ul className="project-details">
-              <li>Developed the 'Ratings &amp; Reviews' section, utilizing dynamic querying to load only specific information requested by the user.</li>
-              <li>Combined 4 modules into a single web app using service-oriented architecture.</li>
-            </ul>
-            <div className="project-links">
-              <a href="http://3.101.29.112/1/" target="_blank">Check it out</a>
-              <a href="https://github.com/All-Day-I-Dream-About-Servers" target="_blank">Github Repo</a>
-              <a href="#top">Back to top</a>
-            </div>
-          </div>
-          <div className="project-footer bg-dark"/>
-        </div>
         <div id="SDC"/>
         <div className="project-item">
           <div className="project-title bg-dark">
@@ -96,6 +82,32 @@ const Projects = (props) => {
             </ul>
             <div className="project-links">
               <a href="https://github.com/10-Million-Dog-Goggles/search-bar-service" target="_blank" className="project-link">Github Repo</a>
+              <a href="#top">Back to top</a>
+            </div>
+          </div>
+          <div className="project-footer bg-dark"/>
+        </div>
+
+        <div id="FEC"/>
+        <div className="project-item">
+          <div className="project-title bg-dark">
+            <h4>All Day I Dream About Servers</h4>
+            <h6 className="project-byline">An adidas product detail page mock-up</h6>
+          </div>
+          <div className="project-image">
+            <img className="project-image" src="https://i.imgur.com/Sc7qASD.gif" alt="adidas product detail page demo" text="adidas product detail page demo" width="100%"></img>
+          </div>
+          <div className="project-description">
+            <div className="project-tech">
+              <span className="tech-title">Tech stack</span> | React, Node + Express, MySQL + Sequelize, Jest + Enzyme, CircleCI
+            </div>
+            <ul className="project-details">
+              <li>Developed the 'Ratings &amp; Reviews' section, utilizing dynamic querying to load only specific information requested by the user.</li>
+              <li>Combined 4 modules into a single web app using service-oriented architecture.</li>
+            </ul>
+            <div className="project-links">
+              <a href="http://3.101.29.112/1/" target="_blank">Check it out</a>
+              <a href="https://github.com/All-Day-I-Dream-About-Servers" target="_blank">Github Repo</a>
               <a href="#top">Back to top</a>
             </div>
           </div>
